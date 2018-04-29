145cb7e859f0159435e188f59f89fdc1952f33a2
Erich Blume <blume.erich@gmail.com>
2018-04-28 17:57:49 -0700

    Thus concludes the factories experiment.
    I tried a few different approaches for creating testable timeaccounts
    and in the end, the regular old fixtures seem perfect.

--- conftest.py
+++ conftest.py
@@ -1,13 +1,8 @@
 import pytest
 
-from tests.factories import make_timeaccount
-
-
-@pytest.fixture
-def complex_account():
-    return make_timeaccount(num_tags=10)
+from tests.test_timeaccount import complex_account
 
 
 @pytest.fixture(autouse=True)
-def add_doctest_fixtures(doctest_namespace, complex_account):
+def add_doctest_fixtures(doctest_namespace):
     doctest_namespace["account"] = complex_account
--- tests/factories.py
+++
@@ -1,5 +0,0 @@
-from hermes import TimeAccount
-
-
-def make_timeaccount(num_tags=0):
-    pass
