c33b13558eb28cd6ce9f4f55f8245034e73b3da6
Erich Blume <blume.erich@gmail.com>
2019-03-12 21:52:52 -0700

    More refactor of the google client model

--- hermes/clients/gcal.py
+++ hermes/clients/gcal.py
@@ -3,7 +3,7 @@ import datetime as dt
 import re
 import warnings
 from pathlib import Path
-from typing import Iterable, Optional, Union
+from typing import Any, Dict, Iterable, Optional, Union
 
 from appdirs import user_data_dir
 
@@ -15,91 +15,39 @@ from googleapiclient.http import build_http
 from oauth2client import client as oauth2_client, file, tools
 
 from ..categorypool import BaseCategoryPool
+from ..span import Span
 from ..tag import Category, Tag
 from ..timespan import BaseTimeSpan, SqliteTimeSpan
 
 
-class GoogleCalendarTimeSpan(BaseTimeSpan):
-    """GoogleCalendar loaded in a Hermes interface.
+class GoogleServiceClient:
+    # Subclasses should change to implement a service
+    SERVICE_NAME = 'DefaultService'  # will be part of a file name
+    SERVICE_SCOPE = 'http://www.example.com/api'
 
-    Use the `load_gcal` class method to load a TimeSpan with tag data directly.
-    By instantiating an object of this class directly, the underlying TimeSpan
-    will be cached for the lifetime of this wrapper. Either way works, pick the
-    flavor you're more comfortable with.
-    """
+    # Probably won't need to change ever
+    SERVICE_APP_NAME = "HermesCLI"
+    SERVICE_APP_AUTHOR = "Hermes"
+    SERVICE_APP_VERSION = "v3"
 
-    DEFAULT_BASE_CATEGORY = Category("GCal", None)
-
-    def __init__(
-        self,
-        begins_at: Optional[dt.datetime] = None,
-        finish_at: Optional[dt.datetime] = None,
-        oauth_config: Optional[Path] = None,
-        base_category: Category = None,
-    ) -> None:
-        self._cached_timespan: Optional[BaseTimeSpan] = None
+    def __init__(self, oauth_config: Optional[Path] = None):
+        self.oauth_config_path = oauth_config
         self._service = None
-
-        self.begins_at: Optional[dt.datetime] = begins_at
-        self.finish_at: Optional[dt.datetime] = finish_at
-        self.oauth_config_path: Optional[Path] = oauth_config
-        self.base_category: Optional[
-            Category
-        ] = base_category or self.DEFAULT_BASE_CATEGORY
-
-    def _warm_cache(self) -> SqliteTimeSpan:
-        cache = self.load_gcal()
-        self._cached_timespan = cache
-        return cache
-
-    @property
-    def span(self):
-        cache = self._warm_cache()
-        return cache.span
-
-    @property
-    def category_pool(self) -> BaseCategoryPool:
-        cache = self._warm_cache()
-        return cache.category_pool
-
-    def iter_tags(self) -> Iterable["Tag"]:
-        cache = self._warm_cache()
-        yield from cache.iter_tags()
-
-    def filter(self, category: Union["Category", str]) -> "BaseTimeSpan":
-        cache = self._warm_cache()
-        return cache.filter(category)
-
-    def reslice(
-        self, begins_at: Optional[dt.datetime], finish_at: Optional[dt.datetime]
-    ) -> "GoogleCalendarTimeSpan":
-        self.begins_at = begins_at
-        self.finish_at = finish_at
-        self._cached_timespan = self.load_gcal()
-        return self
-
-    def calendars(self) -> Iterable[str]:
-        page_token = None
-        while True:
-            calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
-            yield from calendar_list["items"]
-            page_token = calendar_list.get("nextPageToken")
-            if page_token is None:
-                break
 
     @property
     def service(self):  # type?
         if self._service:
             return self._service
 
-        # TODO - figure out a nicer way to handle OAuth cycle
-        appname = "HermesCLI"
-        appauthor = "Hermes"
-        service_name = "calendar"
+        appname = self.SERVICE_APP_NAME
+        appauthor = self.SERVICE_APP_AUTHOR
+        service_name = self.SERVICE_NAME
         version = "v3"
 
         if self.oauth_config_path is None:
             config_dir: str = user_data_dir(appname, appauthor)
+            # Eventually this needs to be replaced with an OAUTH service auth
+            # (or something? maybe this already works??)
             self.oauth_config_path = Path(config_dir) / "gcal.json"
         client_secrets = str(self.oauth_config_path)
 
@@ -110,7 +58,7 @@ class GoogleCalendarTimeSpan(BaseTimeSpan):
         if credentials is None or credentials.invalid:
             flow = oauth2_client.flow_from_clientsecrets(
                 client_secrets,
-                scope="https://www.googleapis.com/auth/calendar.readonly",
+                scope=self.SERVICE_SCOPE,
                 message=tools.message_if_missing(client_secrets),
             )
             credentials = tools.run_flow(flow, token_store)
@@ -123,7 +71,42 @@ class GoogleCalendarTimeSpan(BaseTimeSpan):
         )
         return self._service
 
-    def load_gcal(self) -> SqliteTimeSpan:
+
+class GoogleCalendarClient(GoogleServiceClient):
+    SERVICE_NAME = 'calendar'
+    SERVICE_SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
+
+    DEFAULT_BASE_CATEGORY = Category("GCal", None)
+
+    def __init__(
+        self,
+        begins_at: Optional[dt.datetime] = None,
+        finish_at: Optional[dt.datetime] = None,
+        calendar_id: Optional[str] = None,
+        base_category: Category = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        self.begins_at: Optional[dt.datetime] = begins_at
+        self.finish_at: Optional[dt.datetime] = finish_at
+        self.calendar_id: Optional[str] = calendar_id
+        self.base_category: Optional[Category] = base_category or self.DEFAULT_BASE_CATEGORY
+
+    def calendars(self) -> Iterable[str]:
+        page_token = None
+        while True:
+            calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
+            yield from calendar_list["items"]
+            page_token = calendar_list.get("nextPageToken")
+            if page_token is None:
+                break
+
+    def calendar(self, calendar_id: str = 'primary') -> Dict[str, Any]:
+        # Note that the info returned by calendars() is slightly disjoint.
+        return self.service.calendars().get(calendarId=calendar_id).execute()
+
+    def load_gcal(self, calendar_id: str = 'primary') -> SqliteTimeSpan:
         """Create a TimeSpan from the specified `ouath_config` file.
 
         THIS WILL BLOCK AND REQUIRE USER INPUT on the first time that it is run
@@ -141,65 +124,68 @@ class GoogleCalendarTimeSpan(BaseTimeSpan):
         '/home/erich/.local/share/HermesCLI'
         '/Users/erich/Library/Application Support/HermesCLI'
         """
-        return SqliteTimeSpan(set(self._tag_events_from_service()))
+        return SqliteTimeSpan(set(self._tag_events_from_service(calendar_id)))
 
-    def _tag_events_from_service(self) -> Iterable["Tag"]:
-        for i, calendar in enumerate(self.calendars()):
-            title = calendar.get(
-                "summaryOverride", calendar.get("summary", f"Imported GCal {i}")
+    def _tag_events_from_service(self, calendar_id: str) -> Iterable["Tag"]:
+        calendar = self.calendar(calendar_id)
+        title = calendar.get("summary", f"Imported GCal")
+        title = re.sub(r"[^a-zA-Z0-9:\- ]*", "", title)
+        category = Category(title, self.base_category)
+        # calendar_resource = service.calendars().get(calendarId=calendar['id']).execute()
+        # ^ unclear if there's anything useful there, I think we get the same data from calendarList
+        # details that are different:
+        # * etag is different. Totally unclear what this is.
+        # * missing from calendar_resource: summaryOverride, backgroundColor, defaultReminders, accessRole, foregroundColor, colorId
+        # * (none missing in reverse)
+        # Conclusion: for at least some calendars, we get nothing useful
+        for event in self._retrieve_events_from_calendar(calendar_id):
+            start = event.get("start")
+            end = event.get("end")
+            valid_from = (
+                date_parse(
+                    start.get("dateTime", start.get("date", None)),
+                    ignoretz=True,
+                )
+                if start
+                else None
             )
-            title = re.sub(r"[^a-zA-Z0-9:\- ]*", "", title)
-            category = Category(title, self.base_category)
-            # calendar_resource = service.calendars().get(calendarId=calendar['id']).execute()
-            # ^ unclear if there's anything useful there, I think we get the same data from calendarList
-            # details that are different:
-            # * etag is different. Totally unclear what this is.
-            # * missing from calendar_resource: summaryOverride, backgroundColor, defaultReminders, accessRole, foregroundColor, colorId
-            # * (none missing in reverse)
-            # Conclusion: for at least some calendars, we get nothing useful
-            for event in self._retrieve_events_from_calendar(calendar):
-                start = event.get("start")
-                end = event.get("end")
-                valid_from = (
-                    date_parse(
-                        start.get("dateTime", start.get("date", None)),
-                        ignoretz=True,
-                    )
-                    if start
-                    else None
+            valid_to = (
+                date_parse(
+                    end.get("dateTime", end.get("date", None)), ignoretz=True
                 )
-                valid_to = (
-                    date_parse(
-                        end.get("dateTime", end.get("date", None)), ignoretz=True
-                    )
-                    if end
-                    else None
-                )
-                yield Tag(
-                    name=event.get("summary", event.get("id")),
-                    category=category,
-                    valid_from=valid_from,
-                    valid_to=valid_to,
-                )
+                if end
+                else None
+            )
+            yield Tag(
+                name=event.get("summary", event.get("id")),
+                category=category,
+                valid_from=valid_from,
+                valid_to=valid_to,
+            )
 
-    def _retrieve_events_from_calendar(self, calendar):
+    def _retrieve_events_from_calendar(self, calendar_id: str):
         # https://developers.google.com/calendar/v3/reference/events/list
-        begins_at = self.begins_at
-        finish_at = self.finish_at
+        if self.begins_at is not None:
+            begins_at = self.begins_at.isoformat() + ("Z" if self.begins_at.tzinfo is None else "")
+        else:
+            begins_at = None
+        if self.finish_at is not None:
+            finish_at = self.finish_at.isoformat() + ("Z" if self.finish_at.tzinfo is None else "")
+        else:
+            finish_at = None
+
         page_token = None
         while True:
             query_params = {
-                "calendarId": calendar["id"],
+                "calendarId": calendar_id,
                 "timeZone": "Etc/UTC",  # TODO - figure out how to get this to play nice with dateutil
                 "maxResults": 2500,
             }
             # There are tons of other query params to look in to. Also, live syncing!
-            begins_at_rfc3339 = begins_at.isoformat() + ("Z" if begins_at.tzinfo is None else "")
-            finish_at_rfc3339 = finish_at.isoformat() + ("Z" if finish_at.tzinfo is None else "")
             if begins_at:
-                query_params["timeMin"] = begins_at_rfc3339
+                query_params["timeMin"] = begins_at
             if finish_at:
-                query_params["timeMax"] = finish_at_rfc3339
+                query_params["timeMax"] = finish_at
             if page_token:
                 query_params["pageToken"] = page_token
 
@@ -209,3 +195,38 @@ class GoogleCalendarTimeSpan(BaseTimeSpan):
             page_token = events.get("nextPageToken", None)
             if page_token is None:
                 break
+
+
+class GoogleCalendarTimeSpan(BaseTimeSpan):
+    "Convenience wrapper that bridges from the GoogleCalendarClient to a TimeSpan."
+
+    def __init__(self, client: Optional[GoogleCalendarClient] = None, calendar_id: str = "primary"):
+        if client is None:
+            self.client = GoogleCalendarClient()
+        else:
+            self.client = client
+        self.calendar_id = calendar_id
+
+        if calendar_id is not None:
+            self._cached_timespan = self.client.load_gcal(self.calendar_id)
+        else:
+            self._cached_timespan = SqliteTimeSpan()
+
+        self.client = GoogleCalendarClient()
+
+    @property
+    def span(self) -> Span:
+        return self._cached_timespan.span
+
+    @property
+    def category_pool(self) -> BaseCategoryPool:
+        return self._cached_timespan.category_pool
+
+    def iter_tags(self) -> Iterable["Tag"]:
+        return self._cached_timespan.iter_tags()
+
+    def filter(self, category: Union["Category", str]) -> BaseTimeSpan:
+        return self._cached_timespan.filter(category)
+
+    def reslice(self, begins_at: Optional[dt.datetime], finish_at: Optional[dt.datetime]) -> "BaseTimeSpan":
+        return self._cached_timespan.reslice(begins_at, finish_at)
--- tests/integration/test_gcal.py
+++ tests/integration/test_gcal.py
@@ -1,49 +1,46 @@
 # -*- coding: utf-8 -*-
-import tempfile
 from operator import attrgetter
-from pathlib import Path
 
 from dateutil.parser import parse as date_parse
 
-from hermes.clients.gcal import GoogleCalendarTimeSpan
+from hermes.clients.gcal import GoogleCalendarClient, GoogleCalendarTimeSpan
 
 import pytest
 
 
 @pytest.fixture(scope="module")
-def gcal_may_2018():
-    begin = date_parse("01 May 2018 00:00:00 PDT")
-    finish = date_parse("31 May 2018 23:59:59 PDT")
+def gcal_march_2019():
+    begin = date_parse("01 March 2019 00:00:00 PDT")
+    finish = date_parse("31 March 2019 23:59:59 PDT")
     return GoogleCalendarTimeSpan()[begin:finish]
 
 
-def test_has_events(gcal_may_2018):
-    assert len(gcal_may_2018) == 102  # value is changing :( this needs to be
-    # made less brittle with a special calendar just for testing, or something.
-    tags = list(gcal_may_2018.iter_tags())
+def test_has_events(gcal_march_2019):
+    assert len(gcal_march_2019) == 3
+    tags = list(gcal_march_2019.iter_tags())
     tags_with_concrete_time = [
         t for t in tags if t.valid_from is not None and t.valid_to is not None
     ]
     earliest_time = min(map(attrgetter("valid_from"), tags_with_concrete_time))
     last_time = max(map(attrgetter("valid_to"), tags_with_concrete_time))
-    assert earliest_time >= gcal_may_2018.span.begins_at
-    assert last_time <= gcal_may_2018.span.finish_at
+    assert earliest_time >= gcal_march_2019.span.begins_at
+    assert last_time <= gcal_march_2019.span.finish_at
 
 
 def test_alternate_creation_args():
-    begin = date_parse("01 June 2016 00:00:00 PDT")
-    finish = date_parse("01 June 2016 23:59:59 PDT")
-    tags_a = set(GoogleCalendarTimeSpan(begins_at=begin, finish_at=finish).iter_tags())
+    begin = date_parse("11 March 2019 00:00:00 PDT")
+    finish = date_parse("11 March 2019 23:59:59 PDT")
+    tags_a = set(GoogleCalendarTimeSpan(GoogleCalendarClient(begins_at=begin, finish_at=finish)).iter_tags())
     tags_b = set(GoogleCalendarTimeSpan()[begin:finish].iter_tags())
     assert tags_a == tags_b
-    assert len(tags_a) == 4
+    assert len(tags_a) == 3
 
 
-def test_category_pool(gcal_may_2018):
-    assert len(gcal_may_2018.category_pool) == 7
-    assert "Contacts" in gcal_may_2018.category_pool
+def test_category_pool(gcal_march_2019):
+    assert len(gcal_march_2019.category_pool) == 2
+    assert "GCal/Erich Blume Personal" in gcal_march_2019.category_pool
 
 
-def test_filter(gcal_may_2018):
-    assert len(gcal_may_2018.filter("not a tag")) == 0
-    assert len(gcal_may_2018.filter("GCal")) == 102
+def test_filter(gcal_march_2019):
+    assert len(gcal_march_2019.filter("not a tag")) == 0
+    assert len(gcal_march_2019.filter("GCal")) == 3
