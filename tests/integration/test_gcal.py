72b0b8d0fc6537f60dd9071991764e91df5fc7cd
Erich Blume <blume.erich@gmail.com>
2019-03-18 08:35:07 -0700

    Fixed tests

--- tests/integration/test_gcal.py
+++ tests/integration/test_gcal.py
@@ -9,9 +9,9 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def gcal_march_2019():
-    begin = date_parse("01 March 2019 00:00:00 PDT")
-    finish = date_parse("31 March 2019 23:59:59 PDT")
+def gcal_jan_2019():
+    begin = date_parse("01 January 2019 00:00:00 PDT")
+    finish = date_parse("31 January 2019 23:59:59 PDT")
     return GoogleCalendarTimeSpan.calendar_by_name(
         "Hermes Test", begins_at=begin, finish_at=finish
     )
@@ -35,21 +35,21 @@ def gcal_hermes_test_id():
     raise ValueError("Test Calendar not found!")
 
 
-def test_has_events(gcal_march_2019):
-    assert len(gcal_march_2019) == 3
-    tags = list(gcal_march_2019.iter_tags())
+def test_has_events(gcal_jan_2019):
+    assert len(gcal_jan_2019) == 3
+    tags = list(gcal_jan_2019.iter_tags())
     tags_with_concrete_time = [
         t for t in tags if t.valid_from is not None and t.valid_to is not None
     ]
     earliest_time = min(map(attrgetter("valid_from"), tags_with_concrete_time))
     last_time = max(map(attrgetter("valid_to"), tags_with_concrete_time))
-    assert earliest_time >= gcal_march_2019.span.begins_at
-    assert last_time <= gcal_march_2019.span.finish_at
+    assert earliest_time >= gcal_jan_2019.span.begins_at
+    assert last_time <= gcal_jan_2019.span.finish_at
 
 
 def test_alternate_creation_args(gcal_hermes_test_id):
-    begin = date_parse("01 March 2019 00:00:00 PDT")
-    finish = date_parse("31 March 2019 23:59:59 PDT")
+    begin = date_parse("01 January 2019 00:00:00 PDT")
+    finish = date_parse("31 January 2019 23:59:59 PDT")
     tags_a = set(
         GoogleCalendarTimeSpan(
             GoogleCalendarClient(begins_at=begin, finish_at=finish),
@@ -65,18 +65,18 @@ def test_alternate_creation_args(gcal_hermes_test_id):
     assert len(tags_a) == 3
 
 
-def test_category_pool(gcal_march_2019):
-    assert len(gcal_march_2019.category_pool) == 2
-    assert "GCal/Hermes Test" in gcal_march_2019.category_pool
+def test_category_pool(gcal_jan_2019):
+    assert len(gcal_jan_2019.category_pool) == 2
+    assert "GCal/Hermes Test" in gcal_jan_2019.category_pool
 
 
-def test_filter(gcal_march_2019):
-    assert len(gcal_march_2019.filter("not a tag")) == 0
-    assert len(gcal_march_2019.filter("GCal")) == 3
+def test_filter(gcal_jan_2019):
+    assert len(gcal_jan_2019.filter("not a tag")) == 0
+    assert len(gcal_jan_2019.filter("GCal")) == 3
 
 
-def test_calendar_by_name(gcal_march_2019, gcal_hermes_test_id):
-    assert gcal_march_2019.calendar_id == gcal_hermes_test_id
+def test_calendar_by_name(gcal_jan_2019, gcal_hermes_test_id):
+    assert gcal_jan_2019.calendar_id == gcal_hermes_test_id
 
 
 def test_calendar_span(gcal_feb_02_2019):
@@ -109,7 +109,7 @@ def test_create_event(gcal_feb_02_2019):
 
     # and then clean up
     new_gcal.remove_events(
-        "Test Event 1", after=event.valid_from, before=event.valid_to
+        "Test Event 1", begins_at=event.valid_from, finish_at=event.valid_to
     )
     assert len(new_gcal) == 0
     assert len(gcal_feb_02_2019) == 1
