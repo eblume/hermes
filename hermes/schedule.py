ed36d8d5e118a69b9f5d3b02fab46faa21d24814
Erich Blume <blume.erich@gmail.com>
2019-03-18 03:32:16 -0700

    Switch to ortools for schedule solving
    This is a fairly large change, and marks a huge milestone for hermes!
    
    First, I've switched from `python-constraint` to `ortools`'s Cp-SAT
    solver, which should be far more expressive, powerful, and fast.
    `ortools` is maintained by Google, so hopefully that's a good sign?
    `ortools` seems to have a bit of a 'generational rewrite' problem where
    it seems that every few years they implement a "newer, better" version
    and distribute it alongside the old version in the same package and
    namespace, which is ugly, but the newest Cp-SAT solver seems to be well
    designed with good documentation.
    
    Second, and far more importantly - `schedule.py` now LITERALLY ACTUALLY
    DOES THE THING! If you run `schedule.py` it will generate a working
    schedule and upload it to google calendar!! HOLY COW!!
    
    Right now the schedule is pretty dumb, but hey, it's a starting point!

--- hermes/schedule.py
+++ hermes/schedule.py
@@ -1,202 +1,94 @@
 # -*- coding: utf-8 -*-
-import abc
-from datetime import datetime, time, timedelta
-from itertools import combinations
-from operator import attrgetter
-from typing import Iterator, List, Optional, Tuple
+from datetime import datetime, time, timedelta, timezone
+from typing import List, Optional, Tuple
 
-import constraint as solver
+from ortools.sat.python import cp_model
 
+from .span import Span
 from .tag import Tag
-from .timespan import SqliteTimeSpan
-
-# Notes:
-# a) The python-constraint solver should eventually be removed to use something
-#    that is aware of `datetime` objects natively.
 
 
 class Schedule:
-    def __init__(self) -> None:
-        self.tasks: List[Task] = []
-        self.constraints: List["Constraint"] = []
-        self.today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        self.interval = timedelta(minutes=5)
+    """Class that is used to build (via subclassing) 'schedules'.
 
-    def task(self, task: "Task", between: Optional[Tuple[time, time]] = None) -> None:
+    Schedules here are 'blueprints' that can be given a Span, and will generate
+    tags representing events such that certain constraints (defined by the
+    subclasser) are met. These tags may then be used to populate a timespan -
+    the caller does this.
+
+    Most constraints should be built in the subclassing Schedule's __init__
+    method.
+
+    Remember to call super().__init__() !
+    """
+
+    def __init__(self):
+        self.tasks: List["Task"] = []
+        self.model = cp_model.CpModel()
+
+    def task(
+        self,
+        task: "Task",
+        between: Optional[Tuple[Optional[time], Optional[time]]] = None,
+    ):
         self.tasks.append(task)
-        task.solver_variable = len(self.tasks) - 1
 
-        if between is not None:
-            # Map from times to datetimes
-            dt_between = tuple(
-                map(
-                    lambda x: self.today.replace(
-                        hour=x.hour,
-                        minute=x.minute,
-                        second=x.second,
-                        microsecond=x.microsecond,
-                    ),
-                    between,
-                )
+        # TODO between
+
+    def populate(self, span: Span) -> List[Tag]:
+        if span.begins_at is None or span.finish_at is None:
+            raise ValueError("Schedules must have concrete, finite spans.")
+
+        start_times = [
+            self.model.NewIntVar(
+                int(span.begins_at.timestamp()),  # upper_bound
+                int((span.finish_at - task.duration).timestamp()),  # lower_bound
+                task.name,
             )
-            task.constrain(BetweenConstraint(task.solver_variable, *dt_between))
+            for task in self.tasks
+        ]
 
-    def solve(self) -> "PlannedSchedule":
-        tags = []
-        slots = list(slice_day(self.today, self.interval))
-        # We use the index of the tasks array as variables - dumb, but it works
-        variables = list(range(len(self.tasks)))
+        stop_times = [
+            self.model.NewIntVar(
+                int((span.begins_at + task.duration).timestamp()),  # upper_bound
+                int(span.finish_at.timestamp()),  # lower_bound
+                task.name,
+            )
+            for task in self.tasks
+        ]
 
-        problem = solver.Problem(solver.MinConflictsSolver())
+        intervals = [
+            self.model.NewIntervalVar(
+                start_time, int(task.duration.total_seconds()), stop_time, task.name
+            )
+            for task, start_time, stop_time in zip(self.tasks, start_times, stop_times)
+        ]
 
-        problem.addVariables(variables, slots)
+        # No time overlapping
+        self.model.AddNoOverlap(intervals)
 
-        problem.addConstraint(
-            solver.AllDifferentConstraint()
-        )  # Don't multi-assign slots
+        # And, now for the magic!
+        solver = cp_model.CpSolver()
+        status = solver.Solve(self.model)
 
-        # Add schedule-general constraints
-        for constraint in self.constraints:
-            problem.addConstraint(constraint.constraint, constraint.variables)
+        if status != cp_model.FEASIBLE:
+            # TODO - much better error handling. Also, optional events!
+            raise ValueError("Non-feasible scheduling problem, uhoh!")
 
-        # Add task-specific constraints
-        for task in self.tasks:
-            for constraint in task.constraints:
-                # Maybe this should use the tasks's variable rather than the constraints?
-                # Hard to say
-                problem.addConstraint(constraint.constraint, constraint.variables)
-
-        # Also, don't overlap time assignments
-        # This will eventually need to be redone to handle cases of sub-tasks
-        for var_a, var_b in combinations(variables, 2):
-            task_a = self.tasks[var_a]
-            task_b = self.tasks[var_b]
-
-            def _dont_overlap(timestamp_a: int, timestamp_b: int) -> bool:
-                time_a = datetime.fromtimestamp(timestamp_a)
-                time_b = datetime.fromtimestamp(timestamp_b)
-                b_overlaps = time_a < time_b < time_a + task_a.duration + task_a.gap
-                a_overlaps = time_b < time_a < time_b + task_b.duration + task_b.gap
-                return not b_overlaps and not a_overlaps
-
-            problem.addConstraint(_dont_overlap, (var_a, var_b))
-
-        # Do the math stuff please (wish it was always this easy)
-        solution = problem.getSolution()
-
-        # map solutions to tasks to make tags
-        for variable, slot in solution.items():
-            task = self.tasks[variable]
-            start = datetime.fromtimestamp(slot)
-            stop = start + task.duration
-            tags.append(task.tag(start, stop))
-
-        return PlannedSchedule(*tags)
-
-    def start_time(self, time: time) -> None:
-        when = self.today.replace(
-            hour=time.hour,
-            minute=time.minute,
-            second=time.second,
-            microsecond=time.microsecond,
-        )
-        self.constraints.append(StartTimeConstraint(when))
-
-    def stop_time(self, time: time) -> None:
-        when = self.today.replace(
-            hour=time.hour,
-            minute=time.minute,
-            second=time.second,
-            microsecond=time.microsecond,
-        )
-        self.constraints.append(StopTimeConstraint(when))
-
-
-class PlannedSchedule:
-    def __init__(self, *tags: "Tag") -> None:
-        self.plan = SqliteTimeSpan()
-        for tag in tags:
-            self.plan.insert_tag(tag)
-
-    def print(self) -> None:
-        for i, task in enumerate(
-            sorted(self.plan.iter_tags(), key=attrgetter("valid_from"))
-        ):
-            window = f"{task.valid_from.time().isoformat() if task.valid_from else '...'} to {task.valid_to.time().isoformat() if task.valid_to else '...'}"
-            print(f"[{i+1}] {task.name}: {window}")
+        return [
+            Tag(
+                name=task.name,
+                # TODO - category,
+                valid_from=datetime.fromtimestamp(
+                    solver.Value(start_time), timezone.utc
+                ),
+                valid_to=datetime.fromtimestamp(solver.Value(stop_time), timezone.utc),
+            )
+            for task, start_time, stop_time in zip(self.tasks, start_times, stop_times)
+        ]
 
 
 class Task:
-    def __init__(
-        self,
-        name: str = "Untitled Task",
-        duration: timedelta = timedelta(hours=1),
-        gap: timedelta = timedelta(minutes=5),
-    ) -> None:
-        self.constraints: List["Constraint"] = []
-        self.task_name = name
+    def __init__(self, name: str, duration: timedelta = timedelta(minutes=30)):
+        self.name = name
         self.duration = duration
-        self.gap = gap
-        self.solver_variable: Optional[int] = None
-
-    def __str__(self) -> str:
-        return self.task_name
-
-    def tag(self, start: datetime, stop: datetime) -> Tag:
-        return Tag(name=self.task_name, valid_from=start, valid_to=stop)
-
-    def constrain(self, constraint: "Constraint") -> None:
-        self.constraints.append(constraint)
-
-
-class Constraint(metaclass=abc.ABCMeta):
-    @property
-    def variables(self) -> Optional[List[int]]:
-        return None  # means 'all variables'
-
-    @abc.abstractproperty
-    @property
-    def constraint(self) -> solver.Constraint:
-        raise NotImplementedError("Subclasses must define this interface")
-
-
-class StartTimeConstraint(Constraint):
-    def __init__(self, when: datetime) -> None:
-        self.when = when.timestamp()
-
-    @property
-    def constraint(self) -> solver.Constraint:
-        return solver.FunctionConstraint(lambda *xs: all(x > self.when for x in xs))
-
-
-class StopTimeConstraint(Constraint):
-    def __init__(self, when: datetime) -> None:
-        self.when = when.timestamp()
-
-    @property
-    def constraint(self) -> solver.Constraint:
-        return solver.FunctionConstraint(lambda *xs: all(x < self.when for x in xs))
-
-
-class BetweenConstraint(Constraint):
-    def __init__(self, variable: int, begin: datetime, end: datetime) -> None:
-        self.variable = variable
-        self.begin = begin.timestamp()
-        self.end = end.timestamp()
-
-    @property
-    def constraint(self) -> solver.Constraint:
-        return solver.FunctionConstraint(
-            lambda x: self.begin < x < self.end, [self.variable]
-        )
-
-    @property
-    def variables(self) -> Optional[List[int]]:
-        return [self.variable]
-
-
-def slice_day(day: datetime, interval: timedelta) -> Iterator[float]:
-    cursor = day
-    while cursor - day < timedelta(hours=24):
-        yield cursor.timestamp()
-        cursor += interval
--- schedule.py
+++ schedule.py
@@ -1,46 +1,58 @@
 # -*- coding: utf-8 -*-
-from datetime import time, timedelta
+from datetime import datetime, time, timedelta
 
+from hermes.clients.gcal import GoogleCalendarTimeSpan
 from hermes.schedule import Schedule, Task
+from hermes.span import Span
 
-schedule = Schedule()
 
-# Take medicine
-medicine = Task("Take my medicine", duration=timedelta(minutes=5))
-schedule.task(medicine)
+class DailySchedule(Schedule):
+    def __init__(self):
+        super().__init__()
 
-# Drink metamucil but not within 1 hour of taking medicine
-metamucil = Task("Drink metamucil", duration=timedelta(minutes=10))
-schedule.task(metamucil)
-# schedule.not_within(medicine, metamucil, timedelta(minutes=60))
+        # Take medicine
+        medicine = Task("Take my medicine", duration=timedelta(minutes=5))
+        self.task(medicine)
 
-# Take out trash after 8pm
-trash = Task("Take out the trash", duration=timedelta(minutes=30))
-schedule.task(trash)
+        # Drink metamucil but not within 1 hour of taking medicine
+        metamucil = Task("Drink metamucil", duration=timedelta(minutes=10))
+        self.task(metamucil)
+        # self.not_within(medicine, metamucil, timedelta(minutes=60))
 
-# Eat Breakfast
-breakfast = Task("Eat breakfast", duration=timedelta(minutes=45))
-schedule.task(breakfast, between=(time(hour=8), time(hour=10)))
+        # Take out trash after 8pm
+        trash = Task("Take out the trash", duration=timedelta(minutes=30))
+        self.task(trash)
 
-# Eat Lunch
-lunch = Task("Eat lunch", duration=timedelta(minutes=45))
-schedule.task(lunch, between=(time(hour=12), time(hour=14)))
+        # Eat Breakfast
+        breakfast = Task("Eat breakfast", duration=timedelta(minutes=45))
+        self.task(breakfast, between=(time(hour=8), time(hour=10)))
 
-# Eat Dinner
-dinner = Task("Eat dinner", duration=timedelta(minutes=60))
-schedule.task(dinner, between=(time(hour=19), time(hour=21)))
+        # Eat Lunch
+        lunch = Task("Eat lunch", duration=timedelta(minutes=45))
+        self.task(lunch, between=(time(hour=12), time(hour=14)))
 
-# Do dishes after 8pm and after dinner
-dishes = Task("Do the dishes", duration=timedelta(minutes=15))
-schedule.task(dishes)
+        # Eat Dinner
+        dinner = Task("Eat dinner", duration=timedelta(minutes=60))
+        self.task(dinner, between=(time(hour=19), time(hour=21)))
 
-# Set out clothes for tomorrow by 9pm and after dinner
-clothes = Task("Set out clothes", duration=timedelta(minutes=15))
-schedule.task(clothes)
+        # Do dishes after 8pm and after dinner
+        dishes = Task("Do the dishes", duration=timedelta(minutes=15))
+        self.task(dishes)
 
-schedule.start_time(time(hour=8))  # NB: means 7 o'clock... unsure if I like that
-schedule.stop_time(time(hour=23))
+        # Set out clothes for tomorrow by 9pm and after dinner
+        clothes = Task("Set out clothes", duration=timedelta(minutes=15))
+        self.task(clothes)
 
-plan = schedule.solve()
 
-plan.print()
+start = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+stop = start + timedelta(hours=16)
+
+today = Span(begins_at=start, finish_at=stop)
+daily_schedule = DailySchedule()
+tasks = daily_schedule.populate(today)
+calendar = GoogleCalendarTimeSpan.calendar_by_name("Hermes Test")
+
+for task in tasks:
+    calendar.insert_tag(task)
+
+calendar.flush()
